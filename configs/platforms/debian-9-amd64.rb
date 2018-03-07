a0132802947838f26db8ce567853c0785188e56a
Michael Smith <michael.smith@puppet.com>
2018-03-07 13:38:41 -0800

    (BOLT-374) Add Debian 9 packages

--- configs/platforms/debian-9-amd64.rb
+++ configs/platforms/debian-9-amd64.rb
@@ -1,14 +1,14 @@
-platform "debian-8-i386" do |plat|
+platform "debian-9-amd64" do |plat|
   plat.servicedir "/lib/systemd/system"
   plat.defaultdir "/etc/default"
   plat.servicetype "systemd"
-  plat.codename "jessie"
+  plat.codename "stretch"
 
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-#{plat.get_codename}.deb"
   plat.add_build_repository "http://apt.puppetlabs.com/puppet5-release-#{plat.get_codename}.deb"
   plat.provision_with "curl http://apt.puppetlabs.com/pubkey.gpg | apt-key add - "
 
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot"
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot libssl-dev ruby-full rubygems"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
-  plat.vmpooler_template "debian-8-i386"
+  plat.vmpooler_template "debian-9-x86_64"
 end
