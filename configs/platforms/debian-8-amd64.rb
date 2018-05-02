d892e6deb2926977736e74fb271cf85f6e4f54ed
Michael Smith <michael.smith@puppet.com>
2018-05-02 16:41:36 -0700

    (BOLT-461) Remove unnecessary provision step
    We no longer build against puppet-agent. Stop adding the puppet5 repo
    and necessary gpg key.

--- configs/platforms/debian-8-amd64.rb
+++ configs/platforms/debian-8-amd64.rb
@@ -5,8 +5,6 @@ platform "debian-8-amd64" do |plat|
   plat.codename "jessie"
 
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-#{plat.get_codename}.deb"
-  plat.add_build_repository "http://apt.puppetlabs.com/puppet5-release-#{plat.get_codename}.deb"
-  plat.provision_with "curl http://apt.puppetlabs.com/pubkey.gpg | apt-key add - "
 
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot libssl-dev ruby-full rubygems"
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
--- configs/platforms/el-6-x86_64.rb
+++ configs/platforms/el-6-x86_64.rb
@@ -3,7 +3,6 @@ platform "el-6-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.add_build_repository "http://yum.puppetlabs.com/puppet5/puppet5-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
   plat.provision_with "rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
   plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utilsi autogen"
   plat.install_build_dependencies_with "yum install --assumeyes"
