83bf1bc578deeefa1a88db1a9b3cc06ea4f0050d
Michael Smith <michael.smith@puppet.com>
2018-07-27 14:07:15 -0700

    (BOLT-721) Use server install location
    Change install paths and use GEM_HOME to install to
    /opt/puppetlabs/server/app/bolt-server.

--- configs/components/_base-rubygem.rb
+++ configs/components/_base-rubygem.rb
@@ -1,3 +1,4 @@
+pkg.environment "GEM_HOME", settings[:gem_home]
 gemname = pkg.get_name.gsub('rubygem-', '')
 pkg.url "https://rubygems.org/downloads/#{gemname}-#{pkg.get_version}.gem"
 pkg.mirror "#{settings[:buildsources_url]}/#{gemname}-#{pkg.get_version}.gem"
--- configs/projects/pe-bolt-server.rb
+++ configs/projects/pe-bolt-server.rb
@@ -4,14 +4,13 @@ project "pe-bolt-server" do |proj|
   # We can just have the same version as bolt, and use tags for specific packages
   proj.version_from_git
 
-  #proj.setting(:prefix, "/opt/puppetlabs/server/apps/bolt-server")
-  proj.setting(:prefix, "/opt/puppetlabs/puppet")
+  proj.setting(:prefix, "/opt/puppetlabs/server/apps/bolt-server")
   proj.setting(:sysconfdir, "/etc/puppetlabs/bolt-server")
   proj.setting(:logdir, "/var/log/puppetlabs/bolt-server")
   proj.setting(:bindir, "#{proj.prefix}/bin")
-  proj.setting(:link_bindir, "/opt/puppetlabs/bin")
-  proj.setting(:main_bin, "/usr/local/bin")
-  proj.setting(:gem_install, "/opt/puppetlabs/puppet/bin/gem install --no-rdoc --no-ri --bindir=#{proj.bindir} --local --force ")
+  proj.setting(:libdir, "#{proj.prefix}/lib")
+  proj.setting(:gem_home, File.join(proj.libdir, 'ruby', 'gems', '2.4.0'))
+  proj.setting(:gem_install, "/opt/puppetlabs/puppet/bin/gem install --no-rdoc --no-ri --local --bindir=#{proj.bindir}")
   proj.setting(:gem_build, "/opt/puppetlabs/puppet/bin/gem build")
   proj.setting(:artifactory_url, "https://artifactory.delivery.puppetlabs.net/artifactory")
   proj.setting(:buildsources_url, "#{proj.artifactory_url}/generic/buildsources")
@@ -34,9 +33,6 @@ project "pe-bolt-server" do |proj|
   proj.component 'pe-bolt-server'
 
   proj.directory proj.prefix
-  proj.directory proj.bindir
-  proj.directory proj.link_bindir
-  proj.directory proj.main_bin
   proj.directory proj.sysconfdir
   proj.directory proj.logdir
 end
