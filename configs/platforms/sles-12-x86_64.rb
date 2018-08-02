df6930d1b6b0bb5d6e62c306a6bf0da871c30375
Michael Smith <michael.smith@puppet.com>
2018-08-02 13:06:59 -0700

    (maint) Ensure git is present on builders for r10k
    Builds on Linux VMs just started failing because r10k can no longer find
    git. Update the platform setup to ensure git is present in case we're on
    images that don't have it installed.

--- configs/platforms/sles-12-x86_64.rb
+++ configs/platforms/sles-12-x86_64.rb
@@ -6,7 +6,7 @@ platform "sles-12-x86_64" do |plat|
 
   plat.add_build_repository "http://enterprise.delivery.puppetlabs.net/#{peversion}/repos/#{plat.get_name}/#{plat.get_name}.repo"
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make rpm-build"
+  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make rpm-build git"
   plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
   plat.vmpooler_template "sles-12-x86_64"
 end
