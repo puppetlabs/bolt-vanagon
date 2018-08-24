be5ab0c4f109123ba85fd97d404da4322765bcae
Michael Smith <michael.smith@puppet.com>
2018-08-24 11:12:15 -0700

    (maint) Move service config to packaging, fix GEM_PATH
    Move service config from the bolt repo to packaging.
    
    Also GEM_HOME appears to be appended to GEM_PATH. If a different
    version of Bolt is installed in puppet-agent, that impacts how
    bolt-server functions. Set GEM_PATH instead to ensure we use the
    bolt-server specific gems first.
    
    This ties bolt-server's service config more closely to the puppet-agent
    Ruby setup, since we have to include its GEM_PATH as well.

--- configs/components/pe-bolt-server.rb
+++ configs/components/pe-bolt-server.rb
@@ -16,10 +16,13 @@ component "pe-bolt-server" do |pkg, settings, platform|
 
   case platform.servicetype
   when "systemd"
-    pkg.install_service "ext/systemd/pe-bolt-server.service"
+    pkg.add_source("file://resources/systemd/pe-bolt-server.service", sum: "37bf640ee38c77ee8893badf9af6ad05")
+    pkg.install_service "../pe-bolt-server.service"
   when "sysv"
     if platform.is_rpm?
-      pkg.install_service "ext/redhat/pe-bolt-server.init", "ext/redhat/pe-bolt-server.sysconfig"
+      pkg.add_source("file://resources/redhat/pe-bolt-server.init", sum: "e59aafed20d3db5025f99f6345a3fd29")
+      pkg.add_source("file://resources/redhat/pe-bolt-server.sysconfig", sum: "273ddf6ee45968f2f96a0a7adc3b4a59")
+      pkg.install_service "../pe-bolt-server.init", "../pe-bolt-server.sysconfig"
     else
       fail "This OS is not supported. See https://puppet.com/docs/pe/latest/supported_operating_systems.html#puppet-master-platforms for supported platforms"
     end
