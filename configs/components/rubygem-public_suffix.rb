429b35dbe0fc1e6fd9c925367ad9d0e88413de75
Michael Smith <michael.smith@puppet.com>
2018-08-27 16:41:45 -0700

    (maint) Bump dependencies
    Update to latest bug-fix releases of mustermann, public_suffix, and
    r10k.

--- configs/components/rubygem-public_suffix.rb
+++ configs/components/rubygem-public_suffix.rb
@@ -1,5 +1,5 @@
 component "rubygem-public_suffix" do |pkg, settings, platform|
-  pkg.version "3.0.2"
-  pkg.md5sum "ccf76d402599538452c1166d97f874a6"
+  pkg.version "3.0.3"
+  pkg.md5sum "ed3fcbcfd26093918e0afcc083480403"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
