c5e448d09db16861f6dc4fca698cd620d837f5a4
donoghuc <cas.donoghue@gmail.com>
2018-09-05 16:50:58 -0700

    (BOLT-658) Add json-schema gem
    The json-schema library is used to validate requests for the bolt-server api.

---
+++ configs/components/rubygem-json-schema.rb
@@ -0,0 +1,5 @@
+component "rubygem-json-schema" do |pkg, settings, platform|
+  pkg.version "2.8.0"
+  pkg.md5sum "075b4034f57ee8ab900a92a8da45054a"
+  instance_eval File.read('configs/components/_base-rubygem.rb')
+end
