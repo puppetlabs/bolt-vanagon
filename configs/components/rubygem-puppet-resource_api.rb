1553e3f8219c879ea3100745e84c2b1ca73086c0
Michael Smith <michael.smith@puppet.com>
2018-09-25 14:31:03 -0700

    (BOLT-748) Explicitly install previously vendored dependencies
    Bolt no longer vendors Puppet, Facter, and Hiera. Install them
    explicitly.

---
+++ configs/components/rubygem-puppet-resource_api.rb
@@ -0,0 +1,5 @@
+component "rubygem-puppet-resource_api" do |pkg, settings, platform|
+  pkg.version "1.6.0"
+  pkg.md5sum "845ed7424dabf62a3a3d3acfb3b862a6"
+  instance_eval File.read('configs/components/_base-rubygem.rb')
+end
