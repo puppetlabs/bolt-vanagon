5af6bd27cb283116374fc7d61673aedcdb46161e
Michael Smith <michael.smith@puppet.com>
2018-09-25 14:18:47 -0700

    (BOLT-882) Update dependencies
    Includes a fix for
    https://access.redhat.com/security/cve/cve-2018-1000544 in rubyzip.

--- configs/components/rubygem-sinatra.rb
+++ configs/components/rubygem-sinatra.rb
@@ -1,5 +1,5 @@
 component "rubygem-sinatra" do |pkg, settings, platform|
-  pkg.version "2.0.3"
-  pkg.md5sum "7aa198199e60b4fd6521b39221908501"
+  pkg.version "2.0.4"
+  pkg.md5sum "35eb661337d209865f9ddbd6571973ce"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
