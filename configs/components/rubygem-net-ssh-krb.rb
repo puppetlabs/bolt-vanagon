d766c61c51a95b4bb0f5e76ccc28b273e3084901
Lucy Wyman <wyman.lucy@gmail.com>
2018-10-03 09:16:21 -0700

    (BOLT-879) Remove net-ssh-krb gem
    **What this changes** Removes the net-ssh-krb gem from bolt packages
    **Why** Incompatible licenses for distribution

--- configs/components/rubygem-net-ssh-krb.rb
+++
@@ -1,6 +0,0 @@
-component "rubygem-net-ssh-krb" do |pkg, settings, platform|
-  pkg.version "0.4.0"
-  pkg.md5sum "a19209530717d3b1254aebb2ab230635"
-  instance_eval File.read('configs/components/_base-rubygem.rb')
-end
-
