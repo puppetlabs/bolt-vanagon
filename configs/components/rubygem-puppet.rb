fe0d9a17fa4b2aca60c5056fb27840ca498d5177
Michael Smith <michael.smith@puppet.com>
2018-10-04 11:51:25 -0700

    (maint) Bump Puppet 6.0.1 to 6.0.2 for fixes

--- configs/components/rubygem-puppet.rb
+++ configs/components/rubygem-puppet.rb
@@ -1,5 +1,5 @@
 component "rubygem-puppet" do |pkg, settings, platform|
-  pkg.version "6.0.1"
-  pkg.md5sum "271b04a34964e4db6f029ffd910fe04d"
+  pkg.version "6.0.2"
+  pkg.md5sum "894459f2f68cac1956c93ee0c98f4c34"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
