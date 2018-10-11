fd04403546f46e3269dfa1425117e7c5abeaa965
Michael Smith <michael.smith@puppet.com>
2018-10-11 13:20:25 -0700

    (BOLT-380) Enable ed25519 support in net-ssh
    Not included on Windows yet. The Ruby from puppet-runtime doesn't work
    well for gems with C extensions, and I couldn't get ed25519 to compile.

---
+++ configs/components/rubygem-bcrypt_pbkdf.rb
@@ -0,0 +1,5 @@
+component "rubygem-bcrypt_pbkdf" do |pkg, settings, platform|
+  pkg.version "1.0.0"
+  pkg.md5sum "5ce3ccb9d550b78a8bca4d208f7ee619"
+  instance_eval File.read('configs/components/_base-rubygem.rb')
+end
