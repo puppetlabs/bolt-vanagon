202835008b9a5bf145708e253ef1df8995ca10cc
Michael Smith <michael.smith@puppet.com>
2018-10-17 13:09:45 -0700

    (BOLT-903) Add logrotate config for pe-bolt-server
    Adds log rotation with compression via logrotate. Relies on Puma's
    support for HUP when using `stdout_redirect` (as bolt-server does); see
    https://github.com/puma/puma/blob/v3.12.0/docs/signals.md.
    
    Rotates daily, keeping a 30 day record consistent with pxp-agent.

--- configs/components/pe-bolt-server.rb
+++ configs/components/pe-bolt-server.rb
@@ -16,13 +16,17 @@ component "pe-bolt-server" do |pkg, settings, platform|
 
   case platform.servicetype
   when "systemd"
-    pkg.add_source("file://resources/systemd/pe-bolt-server.service", sum: "37bf640ee38c77ee8893badf9af6ad05")
+    pkg.add_source("file://resources/systemd/pe-bolt-server.service", sum: "b6894840038999f8667197b4733c2c1e")
+    pkg.add_source("file://resources/systemd/pe-bolt-server.logrotate", sum: "e3e77a174da30ab4eb0d55da04f9b31b")
     pkg.install_service "../pe-bolt-server.service"
+    pkg.install_configfile "../pe-bolt-server.logrotate", "/etc/logrotate.d/pe-bolt-server"
   when "sysv"
     if platform.is_rpm?
-      pkg.add_source("file://resources/redhat/pe-bolt-server.init", sum: "e59aafed20d3db5025f99f6345a3fd29")
+      pkg.add_source("file://resources/redhat/pe-bolt-server.init", sum: "48ca55616c992cc57405c6e6e2824ecb")
       pkg.add_source("file://resources/redhat/pe-bolt-server.sysconfig", sum: "273ddf6ee45968f2f96a0a7adc3b4a59")
+      pkg.add_source("file://resources/redhat/pe-bolt-server.logrotate", sum: "47740a40b4c22b7d6129c51f03d14c96")
       pkg.install_service "../pe-bolt-server.init", "../pe-bolt-server.sysconfig"
+      pkg.install_configfile "../pe-bolt-server.logrotate", "/etc/logrotate.d/pe-bolt-server"
     else
       fail "This OS is not supported. See https://puppet.com/docs/pe/latest/supported_operating_systems.html#puppet-master-platforms for supported platforms"
     end
