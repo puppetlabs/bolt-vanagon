a0aeaf8f2e56e039825b8677fab86cfddbc42da3
Alex Dreyer <alex@puppetlabs.com>
2019-03-01 13:43:29 -0800

    (BOLT-645) bump puppet-resource_api to support remote transports

--- configs/components/rubygem-puppet-resource_api.rb
+++ configs/components/rubygem-puppet-resource_api.rb
@@ -1,5 +1,5 @@
 component "rubygem-puppet-resource_api" do |pkg, settings, platform|
-  pkg.version "1.6.2"
-  pkg.md5sum "ba038d56be25c55affa2bda964acce34"
+  pkg.version "1.8.0"
+  pkg.md5sum "f5f8d8414c27f6f015b70f12a0c27fe83878bbac346746d82c27858acf7378f5"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
