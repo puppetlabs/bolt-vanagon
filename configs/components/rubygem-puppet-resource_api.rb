a5fe04c5c1d8500de09fd8698b0695fbd6b81ab2
Cas Donoghue <cas.donoghue@gmail.com>
2019-03-04 13:13:45 -0800

    Update rubygem-puppet-resource_api.rb

--- configs/components/rubygem-puppet-resource_api.rb
+++ configs/components/rubygem-puppet-resource_api.rb
@@ -1,5 +1,5 @@
 component "rubygem-puppet-resource_api" do |pkg, settings, platform|
   pkg.version "1.8.0"
-  pkg.md5sum "f5f8d8414c27f6f015b70f12a0c27fe83878bbac346746d82c27858acf7378f5"
+  pkg.md5sum "d40e587b661883e0e3cd11bd3754c4b0"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
