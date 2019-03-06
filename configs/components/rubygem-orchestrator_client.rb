d230d094275ba7b7ff862483921d572accd7c2bc
donoghuc <cas.donoghue@gmail.com>
2019-03-05 18:02:49 -0800

    (BOLT-1076) Bump orchestrator_client gem
    Pull in orchestrator_client gem that contains enhancement whereby an error with a helpful message is raised when an invalid character is detected in a token file or if the token-file is unreadable. The new version also requires two new (to Bolt) gems that allow persistent http connections when using linux Bolt controller.

--- configs/components/rubygem-orchestrator_client.rb
+++ configs/components/rubygem-orchestrator_client.rb
@@ -1,5 +1,5 @@
 component "rubygem-orchestrator_client" do |pkg, settings, platform|
-  pkg.version "0.3.1"
-  pkg.md5sum "4d5dfb212be076c7a6dcdadb4a2ad7b5"
+  pkg.version "0.4.2"
+  pkg.md5sum "a2c95a3fe3ab736092dc857a44a296a5"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
