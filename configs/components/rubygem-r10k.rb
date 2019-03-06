88c978bca91ef57123335ce83f39a5cd2fab6c54
donoghuc <cas.donoghue@gmail.com>
2019-03-06 13:36:28 -0800

    (BOLT-1106) Bump r10k major version
    Incorporate bug fixes and new features from r10k 3.1.1 into bolt packages. The `cri` gem also needs to be updated so satisfy new r10k version.

--- configs/components/rubygem-r10k.rb
+++ configs/components/rubygem-r10k.rb
@@ -1,5 +1,5 @@
 component "rubygem-r10k" do |pkg, settings, platform|
-  pkg.version "2.6.5"
-  pkg.md5sum "cdafd1663ab651ef06c1d28c30827521"
+  pkg.version "3.1.1"
+  pkg.md5sum "767b7a4b90bcb25fabb6edc424364514"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
