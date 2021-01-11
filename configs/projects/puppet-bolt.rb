e8b07c59aabda8bbb8d9a14a83983e2145e964d1
James Pogran <jpogran@outlook.com>
2021-01-11 15:38:05 -0500

    (maint) Migrate Bolt to bolt.bat
    Migrates Bolt from the `bolt` PowerShell function to a `bolt.bat` file and adds a WiX component that appends the path to Bolt's bin directory to the system PATH Environment Variable.
    
    This enables Bolt to be used in any shell on a Windows Platform, even Cygwin and Git Bash, not just PowerShell.
    
    Requires work done in https://github.com/puppetlabs/bolt/pull/2532 to be complete.

--- configs/projects/puppet-bolt.rb
+++ configs/projects/puppet-bolt.rb
@@ -39,6 +39,8 @@ project "puppet-bolt" do |proj|
       :ManualLink => "https://puppet.com/docs/bolt/",
     })
     proj.setting(:LicenseRTF, "wix/license/LICENSE.rtf")
+    proj.setting(:install_root, File.join("C:", proj.base_dir, proj.company_id, proj.product_id))
+    proj.setting(:link_bindir, File.join(proj.install_root, "bin"))
 
     module_directory = File.join(proj.datadir.sub(/^.*:\//, ''), 'PowerShell', 'Modules')
     proj.extra_file_to_sign File.join(module_directory, 'PuppetBolt', 'PuppetBolt.psm1')
@@ -46,10 +48,10 @@ project "puppet-bolt" do |proj|
     proj.signing_hostname 'windowssigning-aio1-prod.delivery.puppetlabs.net'
     proj.signing_username 'Administrator'
     proj.signing_command 'pwsh.exe -File pwsh7.ps1 -FilePath'
+  else
+    proj.setting(:link_bindir, "/opt/puppetlabs/bin")
+    proj.setting(:main_bin, "/usr/local/bin")
   end
-
-  proj.setting(:link_bindir, "/opt/puppetlabs/bin")
-  proj.setting(:main_bin, "/usr/local/bin")
 
   proj.component "bolt-runtime"
   proj.component "bolt"
