d49f293956286b6764cd6c75625f8f9cbfc6c89d
Jacklyn Kinsler <jacklyn.kinsler@puppet.com>
2021-03-31 15:12:07 -0700

    (VANAGON-85) Move platform definitions into core vanagon. Inherit definitions from a default platform.

--- configs/platforms/el-7-x86_64.rb
+++ configs/platforms/el-7-x86_64.rb
@@ -1,11 +1,5 @@
 platform "el-7-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  plat.provision_with "rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.provision_with "yum install --assumeyes autoconf automake rsync gcc createrepo make rpmdevtools rpm-libs yum-utils rpm-sign libtool autogen git"
-  plat.install_build_dependencies_with "yum install --assumeyes"
-  plat.vmpooler_template "centos-7-x86_64"
+  plat.inherit_from_default
+  packages = %w(git)
+  plat.provision_with("yum install -y #{packages.join(' ')}")
 end
