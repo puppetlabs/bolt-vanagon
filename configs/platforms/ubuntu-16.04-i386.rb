2d45a06aedcbee93bd922602425fcf29b87a90c3
Tom Beech <beech.tom@gmail.com>
2022-06-21 16:16:18 -0700

    (maint) Remove EOL platforms
    This removes Fedora 32 and Ubuntu 16.04 as supported platforms.

--- configs/platforms/ubuntu-16.04-i386.rb
+++
@@ -1,5 +0,0 @@
-platform "ubuntu-16.04-i386" do |plat|
-  plat.inherit_from_default
-  packages = %w(git)
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
-end
