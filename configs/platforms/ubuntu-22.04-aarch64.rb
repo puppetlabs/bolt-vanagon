e0ea8739cc80388a9cbfbfa50c7a45ef9e1a5de2
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2023-11-20 11:31:54 -0600

    (maint) add Ubuntu 22.04 aarch64 platform

---
+++ configs/platforms/ubuntu-22.04-aarch64.rb
@@ -0,0 +1,5 @@
+platform "ubuntu-22.04-aarch64" do |plat|
+  plat.inherit_from_default
+  packages = %w(git)
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+end
